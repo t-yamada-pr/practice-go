e6980a852cd3e28683ab5f02b9dee7d8adb0e854
Tsukasa Yamada <yamada.tsukasa@panair.jp>
2020-04-16 23:18:20 +0900

    fizzbuzz

---
+++ fizzbuzz.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func getStdin() string {
+	stdin := bufio.NewScanner(os.Stdin)
+	stdin.Scan()
+	return stdin.Text()
+}
+
+func fizz(x int) string {
+	if x % 3 == 0 {
+		return "Fizz"
+	}
+	return ""
+}
+
+func buzz(x int) string {
+	if x % 5 == 0 {
+		return "Buzz"
+	}
+	return ""
+}
+
+func main()  {
+	n, _ := strconv.Atoi(getStdin())
+	fmt.Printf(fizz(n)+buzz(n))
+}
